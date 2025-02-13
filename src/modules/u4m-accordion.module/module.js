936a36700cfcf6df6b5158ff79beefb7e8901cf0
amitkmr-123 <amit.kumar@thewebplant.in>
2025-02-13 12:40:25 +0530

    Accodion Module -- Done

--- src/modules/u4m-accordion.module/module.js
+++ src/modules/u4m-accordion.module/module.js
@@ -1,4 +1,22 @@
-$('.accTtl').click(function () {
-  $(this).next('.accContent').slideToggle(500);
-  $(this).parent().toggleClass('active');
-})
+$(document).ready(function () {
+    $(".title").click(function () {
+        var $parent = $(this).closest(".items");
+        var $content = $parent.find(".content");
+        var $pd = $parent.find(".pd");
+
+        // Check if the clicked item is already active
+        var isActive = $(this).hasClass("active");
+
+        // Remove 'active' class from all titles, content, and 'accActive' from .pd
+        $(".items .title").removeClass("active");
+        $(".items .content").slideUp().removeClass("active");
+        $(".items .pd").removeClass("accActive");
+
+        // If it wasn't active before, activate it
+        if (!isActive) {
+            $(this).addClass("active");
+            $content.slideDown().addClass("active");
+            $pd.addClass("accActive");
+        }
+    });
+});
