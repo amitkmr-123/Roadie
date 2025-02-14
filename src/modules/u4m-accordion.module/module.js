f7de7190642e5ff49a61eba0b9c157b878553c9d
amitkmr-123 <amit.kumar@thewebplant.in>
2025-02-14 11:32:51 +0530

    Cta Row -- Done

--- src/modules/u4m-accordion.module/module.js
+++ src/modules/u4m-accordion.module/module.js
@@ -1,23 +1,23 @@
 $(document).ready(function () {
-    $(".title").click(function () {
-        var $parent = $(this).closest(".items");
-        var $content = $parent.find(".content");
-        var $pd = $parent.find(".pd");
+	$(".title").click(function () {
+		var $parent = $(this).closest(".items");
+		var $content = $parent.find(".content");
+		var $pd = $parent.find(".pd");
 
-        // Check if the clicked item is already active
-        var isActive = $(this).hasClass("active");
+		// Check if the clicked item is already active
+		var isActive = $(this).hasClass("active");
 
-        // Remove 'active' class from all titles, content, and 'accActive' from .pd
-        $(".items .title").removeClass("active");
-        $(".items .content").slideUp().removeClass("active");
-        $(".items .pd").removeClass("accActive");
+		// Remove 'active' class from all titles, content, and 'accActive' from .pd
+		$(".items .title").removeClass("active");
+		$(".items .content").slideUp().removeClass("active");
+		$(".items .pd").removeClass("accActive");
 
-        // If it wasn't active before, activate it
-        if (!isActive) {
-            $(this).addClass("active");
-            $content.slideDown().addClass("active");
-            $pd.addClass("accActive");
-        }
-    });
+		// If it wasn't active before, activate it
+		if (!isActive) {
+			$(this).addClass("active");
+			$content.slideDown().addClass("active");
+			$pd.addClass("accActive");
+		}
+	});
 });
 
