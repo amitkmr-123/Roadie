93657207dd9eedc5c0afa89e3e6e8993db87aa39
SanjaySinghTWP <sanjays@thewebplant.in>
2025-02-17 17:57:37 +0530

    Top Tab Module Done

---
+++ src/modules/u4m-top-tab.module/module.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const tabLinks = document.querySelectorAll(".tab-nav a");
+  const tabContents = document.querySelectorAll(".tab_content");
+
+  tabLinks.forEach((tab) => {
+    tab.addEventListener("click", function () {
+      const tabId = this.getAttribute("tab-id");
+
+      // Remove active class from all tabs and contents
+      tabLinks.forEach((link) => link.parentElement.classList.remove("active"));
+      tabContents.forEach((content) => content.classList.remove("active"));
+
+      // Add active class to the clicked tab and corresponding content
+      this.parentElement.classList.add("active");
+      document
+        .querySelector(`.tab_content[content-id="${tabId}"]`)
+        .classList.add("active");
+    });
+  });
+});
+
+$(".u4m-top-tab .page-center .tabber_section .tab-nav > ul > li > a").click(
+  function () {
+    $(this)
+      .next()
+      .slideToggle(250)
+      .parent()
+      .siblings()
+      .children(".toggle_content")
+      .slideUp(250);
+    $(this).parent().toggleClass("active").siblings().removeClass("active");
+  }
+);
